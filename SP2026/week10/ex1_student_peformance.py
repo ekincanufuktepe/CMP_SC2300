c268cc423064d673ef6ff0a2faab69062ac70a67
ekincanufuktepe <ekincanufuktepe@gmail.com>
2026-04-13 09:51:08 -0500

    Storytelling example 1 added

---
+++ SP2026/week10/ex1_student_peformance.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.DataFrame({
+    "study_hours": [1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 9, 10],
+    "exam_score":  [52, 55, 57, 60, 63, 65, 68, 72, 74, 78, 82, 85, 88],
+    "attendance":  [70, 75, 72, 78, 80, 82, 85, 88, 90, 91, 93, 95, 96]
+})
+
+
+# Scatter plot (boring version)
+
+# Look for correlation
+
+
+# show regression plot
+
+
+# show scatter relationships
+
+
+# show histogram
+
+
+# show regression plot annotate
